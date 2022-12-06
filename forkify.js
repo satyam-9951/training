60244b981952ea0332750a5c2297b03f7c9ecec6
satyam-9951 <satyam-9951>
2022-12-06 15:14:50 +0530

    page nation completed

--- forkify.js
+++ forkify.js
@@ -2,30 +2,47 @@ var prevpage=document.querySelector(".backward");
 var nextpage=document.querySelector(".forward");
 var view=document.getElementById("sathya");
 var find=document.querySelector(".search");
-//find.addEventListener("click",searching);
+const mainimg=document.querySelector(".items");
 
-
-for(var i=0;i<9;i++){
+var pages=1;
+displaying();
+function displaying(){
+for(var i=(pages*9)-9;i<pages*9;i++){
 
     view.innerHTML+=`<li>
         <div  class="list">
                     <div class="pic"><img class="item" src="${info.recipes[i].image_url}"></div>
-                    <div class="dis"><p class="listhead">${info.recipes[i].title}</p><p class="dec">${info.recipes[i].publisher}</p></div>
+                    <div class="dis"><p class="listhead">${info.recipes[i].title}</p>
+					<p class="dec">${info.recipes[i].publisher}</p></div>
         </div>
-    </li>`	
+    </li>`
 }
 
-nextpage.addEventListener("click",pagenotation);
- function pagenotation(i){
-    console.log(i);
-    i+=9;
-    console.log(i);
+if((pages*9)==9){
+		 prevpage.style.display="none";
+		 nextpage.style.display="block";
+	 }
+	 else if((pages*9)>=27 ){
+		 prevpage.style.display="block";
+		 nextpage.style.display="none";
+	 }
+	 else if( (pages*9)>9 ){
+		 prevpage.style.display="block";
+		 nextpage.style.display="block";
+	 }
 
 }
 
-prevpage.addEventListener("click",pagenotation);
- function pagenotation(i){
-    console.log(i);
-    i+=9;
-    console.log(i);
+nextpage.addEventListener("click",forwardpage);
+ function forwardpage(i){
+	 view.innerHTML="";
+	pages++;
+	displaying();
+}
+
+prevpage.addEventListener("click",backwardpage);
+ function backwardpage(i){
+	 view.innerHTML="";
+	 pages--;
+	 displaying();
 }
